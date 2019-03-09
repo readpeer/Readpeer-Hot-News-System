1385224cbed1865e211fbb9ecebe5ed0e382e68d
pengshuyuan <1109400576@qq.com>
2019-03-09 10:27:13 +0800

    first

---
+++ legaldocument/src/main/java/cn/xmu/edu/legaldocument/LegaldocumentApplication.java
@@ -0,0 +1,13 @@
+package cn.xmu.edu.legaldocument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegaldocumentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegaldocumentApplication.class, args);
+    }
+
+}
